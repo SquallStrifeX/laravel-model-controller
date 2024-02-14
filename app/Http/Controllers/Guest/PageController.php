4c175d978f4c1ed3052dc4f9ad5ebabdd626838a
SquallStrifeX <squallstrifex@gmail.com>
2024-02-14 17:39:34 +0100

    db added

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index(){
+        $movies = Movie::orderBy('title')->get();
+        return view('movies.index', compact('movies'));
+    }
+
+public function show($id){
+$movies = Movie::all();
+$movie = $movies->find($id);
+return view("movies.show", compact("movie"));
+}
+}
